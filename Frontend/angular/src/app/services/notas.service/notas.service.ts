2e089c38b3a0501cb3e062535c65311c920f7375
smuneram98 <smm980531@gmail.com>
2026-03-20 18:32:44 -0500

    front (falta)

--- Frontend/angular/src/app/services/notas.service/notas.service.ts
+++ Frontend/angular/src/app/services/notas.service/notas.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { NotasModel } from '../../models/notas.model/notas.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class NotasService {}
+export class NotasService {
+  private url="http://localhost:8080/notas";
+
+  constructor(private http:HttpClient){}
+
+  getNotas(){
+    return this.http.get<NotasModel>(this.url);
+  }
+
+}
--- Frontend/angular/src/app/view/notas.component/notas.component/notas.component.ts
+++ Frontend/angular/src/app/view/notas.component/notas.component/notas.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NotasModel } from '../../../models/notas.model/notas.model';
+import { NotasService } from '../../../services/notas.service/notas.service';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-notas.component',
@@ -6,4 +9,14 @@ import { Component } from '@angular/core';
   templateUrl: './notas.component.html',
   styleUrl: './notas.component.css',
 })
-export class NotasComponent {}
+export class NotasComponent {
+   notas: NotasModel[]=[];
+
+  constructor(private notasService:NotasService, private changeD:ChangeDetectorRef){
+    this.notasService.getNotas().subscribe(data=>{
+      this.notas=data;
+      console.log(this.notas);
+      this.changeD.detectChanges();
+    });
+  }
+}
